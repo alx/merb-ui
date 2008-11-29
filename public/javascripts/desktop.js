f754ecfdc07bdee88c2caac9793cb2069f3aa084
uipoet <dont.tase@me.com>
2008-11-29 11:38:20 -0800

    Windows will no longer re-center when a window is already open. Title bars of windows have correct padding on the left. Image paths within the css view now use image path helper.

--- public/javascripts/desktop.js
+++ public/javascripts/desktop.js
@@ -1,3 +1,6 @@
+// Set the status of the window. 0 == closed, 1 == opened
+var windowStatus = 0;
+
 // Position message at top center.
 function messagePosition(){
   var browserWidth = $(window).width();
@@ -7,41 +10,42 @@ function messagePosition(){
 
 // Close window.
 function windowClose(url){
-  var target = $('.mui_window_target');
-  target.fadeOut(function(){
-	  target.empty;
-    $('.mui_focus:first').focus();
+  $('.mui_window_target').fadeOut(function(){
+    windowStatus = 0;
 	});
 }
 
 // Open window and give the first input with class of mui_focus focus, if one exists.
 function windowOpen(url){
-  var target = $('.mui_window_target');
-  target.fadeOut(function(){
-	  target.load(url, function(){
-	    target.fadeIn();
-      $('.mui_focus:first').focus();
-  	});
+  var target = $('.mui_window_target')
+  target.load(url, function(){
+    if(windowStatus == 0){
+      target.fadeIn();
+  	}
+    $('.mui_focus:first').focus();
+    windowStatus = 1;
 	});
 }
 
 // Position window at middle center.
 function windowPosition(){
-  var browserWidth = $(window).width();
-  var browserHeight = $(window).height();
-  var windowWidth = $('.mui_window_target').width();
-  var windowHeight = $('.mui_window_target').height();
-  if (windowWidth > browserWidth){
-    $('.mui_window_target').css({position:'absolute', left:'1em'});
-  }
-  else{
-    $('.mui_window_target').css({position:'fixed', left:(browserWidth/2)+(windowWidth/2)*(-1)});
-  }
-  if (windowHeight > browserHeight){
-    $('.mui_window_target').css({position:'absolute', top:'1em'});
-  }
-  else{
-    $('.mui_window_target').css({position:'fixed', top:(browserHeight/2)+(windowHeight/2)*(-1)});
+  if(windowStatus == 0){
+    var browserWidth = $(window).width();
+    var browserHeight = $(window).height();
+    var windowWidth = $('.mui_window_target').width();
+    var windowHeight = $('.mui_window_target').height();
+    if (windowWidth > browserWidth){
+      $('.mui_window_target').css({position:'absolute', left:'1em'});
+    }
+    else{
+      $('.mui_window_target').css({position:'fixed', left:(browserWidth/2)+(windowWidth/2)*(-1)});
+    }
+    if (windowHeight > browserHeight){
+      $('.mui_window_target').css({position:'absolute', top:'1em'});
+    }
+    else{
+      $('.mui_window_target').css({position:'fixed', top:(browserHeight/2)+(windowHeight/2)*(-1)});
+    }
   }
 }
 
